5437921f6d49bce43fd9ad07ba7a1bb295695a90
Chernysheva Sofya <128961179+SChernysheva@users.noreply.github.com>
2023-09-30 11:12:09 +0300

    add functions

--- laba1_4/laba1_4.c
+++ laba1_4/laba1_4.c
@@ -5,6 +5,108 @@
 #include <ctype.h>
 #include <limits.h>
 
+
+int flagA(FILE* in, FILE* out){
+    char c;
+    while ((c = fgetc(in)) != EOF)
+        {
+            if (c > 33 && (c < '0' || c > '9')) 
+            {
+                fprintf(out, "%X", c);
+            }
+            else fputc(c, out);
+        }
+    if(c == EOF)
+    {
+        if (feof(in) == 0)
+        {
+            fclose(in);
+            fclose(out);
+            return -1;
+        }
+    }
+    fclose(in);
+    fclose(out);
+    return 1;
+}
+
+int flagS(FILE* in, FILE* out){
+    char c;
+    int count = 0;
+    while ((c = fgetc(in)) != EOF)
+    {
+        if (c == '\n') 
+        {
+            fputc(count+'0', out);
+            fputc(c, out);
+            count = 0;
+        }
+        if ( (c >= 33 && c <= 47) || (c >= 58 && c <= 64) || (c >= 91 && c <= 96) || ( c>= 123 && c <= 176)) {count++;} 
+    }
+    if(c == EOF)
+    {
+        if (feof(in) == 0)
+        {
+            fclose(in);
+            fclose(out);
+            return -1;
+        }
+    }
+    fclose(in);
+    fclose(out);
+    return 1;
+}
+
+int flagI(FILE* in, FILE* out){
+    char c;
+    int count = 0;
+    while ((c = fgetc(in)) != EOF)
+    {
+        if (c == '\n') 
+        {
+            fputc(count+'0', out);
+            fputc(c, out);
+            count=0;
+        }
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {count++;}
+    }
+    if(c == EOF)
+    {
+        if (feof(in) == 0)
+        {
+            fclose(in);
+            fclose(out);
+            return -1;
+        }
+    }
+    fclose(in);
+    fclose(out);
+    return 1;
+}
+
+
+int flagD(FILE* in, FILE* out){
+    char c;
+    while ((c = fgetc(in)) != EOF)
+    {
+            if (c < '0' || c > '9')
+            {
+                fputc(c, out);
+            }
+    }
+    if (c == EOF){
+        if (feof(in) == 0)
+        {
+            fclose(in);
+            fclose(out);
+            return -1;
+        }
+    }
+    fclose(in);
+    fclose(out);
+    return 1;
+}
+
 int main(int argc, char*argv[])
 {
     if( ((argc!=4) && (argc!=3)) || ((strlen(argv[1])!=2) && strlen(argv[1])!=3) || ((argv[1][0]!='-') && (argv[1][0]!='/')))
@@ -21,7 +123,7 @@ int main(int argc, char*argv[])
         printf("Не удалось открыть входной файл.\n");
         return 0;
     }
-    if (argv[1][1]=='n')
+    if (argv[1][1] == 'n')
     {
         checkN=1;
         if (((argv[1][2] != 'd') && (argv[1][2] != 'i') && (argv[1][2] != 's') && (argv[1][2] != 'a')) || argc != 4)
@@ -46,7 +148,7 @@ int main(int argc, char*argv[])
             printf("Возможные флаги: -d, -i, -s, -a \n");
             return 0;
         }
-        char *fout=malloc((size_t)strlen("_out")+(size_t)(strlen(argv[2])+(size_t)strlen(".txt")+1));
+        char *fout=(char*)malloc((size_t)strlen("_out") + (size_t)(strlen(argv[2]) + (size_t)strlen(".txt")+1));
         if (!fout) {
             printf("Не удалось создать файл результата.\n");
             fclose(in);
@@ -54,129 +156,50 @@ int main(int argc, char*argv[])
         }
         memcpy(fout, argv[2], strlen(argv[2]));
         memcpy(fout+strlen(argv[2]), "_out.txt", 8);
-        fout[8+strlen(argv[2])]='\0';
+        fout[8+strlen(argv[2])] = '\0';
         if(!(out = fopen(fout, "w"))) 
         {
-            printf("Не удалось создать файл результата.\n");
+            printf("Не удалось создать файл результата1.\n");
             fclose(in);
             return 0;
         }
         free(fout);
     }
 
-    int indexF=1;
-    if(checkN==1) {indexF=2;}
+    int indexF = 1;
+    if(checkN == 1) {indexF = 2;}
     
-    if(argv[1][indexF]=='d')
+    if(argv[1][indexF] == 'd')
     {
-        char c;
-        while (1)
-        {
-            c=fgetc(in);
-            if(c==EOF)
-            {
-                if (feof(in)==0)
-                {
-                    printf("Ошибка чтения из файла.\n");
-                    break;
-                    fclose(in);
-                    fclose(out);
-                    return 0;
-                }
-                break;
-            }
-            if (c<'0' || c>'9')
-            {
-                fputc(c, out);
-            }
+        if (flagD(in, out) == -1) {
+            printf("Ошибка чтения из файла\n");
+            return 0;
         }
     }
 
-    if(argv[1][indexF]=='i')
+    if(argv[1][indexF] == 'i')
     {
-        char c;
-        int count=0;
-        while (1)
-        {
-            c=fgetc(in);
-            if(c==EOF)
-            {
-                if (feof(in)==0)
-                {
-                    printf("Ошибка чтения из файла.\n");
-                    break;
-                    fclose(in);
-                    fclose(out);
-                    return 0;
-                }
-                break;
-            }
-            if (c=='\n') 
-            {
-                fputc(count+'0', out);
-                fputc(c, out);
-                count=0;
-            }
-            if ((c>='a' && c<='z') || (c>='A' && c<='Z')) {count++;}
+        if (flagI(in, out) == -1) {
+            printf("Ошибка чтения из файла\n");
+            return 0;
         }
     }
 
-    if(argv[1][indexF]=='s')
+    if(argv[1][indexF] =='s')
     {
-        char c;
-        int count=0;
-        while (1)
-        {
-            c=fgetc(in);
-            if(c==EOF)
-            {
-                if (feof(in)==0)
-                {
-                    printf("Ошибка чтения из файла.\n");
-                    break;
-                    fclose(in);
-                    fclose(out);
-                    return 0;
-                }
-                break;
-            }
-            if (c == '\n') 
-            {
-                fputc(count+'0', out);
-                fputc(c, out);
-                count=0;
-            }
-            if ( (c >= 33 && c <= 47) || (c >= 58 && c <= 64) || (c >= 91 && c <= 96) || ( c>= 123 && c <= 176)) {count++;} 
+        if (flagS(in, out) == -1) {
+            printf("Ошибка чтения из файла\n");
+            return 0;
         }
     }
 
-    if(argv[1][indexF]=='a')
+    if(argv[1][indexF] == 'a')
     {
-        char c;
-        while (1)
-        {
-            c=fgetc(in);
-            if(c == EOF)
-            {
-                if (feof(in) == 0)
-                {
-                    printf("Ошибка чтения из файла.\n");
-                    break;
-                    fclose(in);
-                    fclose(out);
-                    return 0;
-                }
-                break;
-            }
-            if (c > 33 && (c < '0' || c > '9')) 
-            {
-                fprintf(out, "%X", c);
-            }
-            else fputc(c, out);
+        if (flagA(in, out) == -1) {
+            printf("Ошибка чтения из файла\n");
+            return 0;
         }
     }
-    fclose(in);
-    fclose(out);
 
 return 0;
 }
