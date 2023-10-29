6388e6ae07b4e943c621695cc1f86f6f13e7d48a
Chernysheva Sofya <128961179+SChernysheva@users.noreply.github.com>
2023-10-29 22:55:38 +0300

    еще раз change the algorithm

--- laba2/laba2_9/laba2_9.c
+++ laba2/laba2_9/laba2_9.c
@@ -21,13 +21,47 @@ struct res
     int ans;
     double num;
 };
+int find_NOD(int a, int b)
+{
+    while (b != 0)
+    {
+        int c = a % b;
+        a = b;
+        b = c;
+    }
+    return a;
+}
+int isPrime(int num)
+{//0 - простое 1- составное
+    if (num==1 || num==0) return -1;
+    if (num==2) return 0;
+    if (num%2==0) return 1;
+    int sqr=sqrt(num);
+
+    for (int i=3; i<=sqr; i+=2){
+        if (num%i==0){
+            return 1;
+        }
+    }
+    return 0;
+}
+enum ans check_prime_devisors(int denominator, int base) //base% den
+{
+    for (int i = 2; i <= denominator / 2; i++)
+    {
+        if (denominator % i == 0 && isPrime(i) == 0)
+        {
+            //printf("%d %d %d\n", denominator, i, base);
+            if (base % i != 0) 
+            {
+                return No;
+            }
+        }
+    }
+    return Yes;
+}
 enum ans checkNum(double num, int base)
-{ // Находим остаток от деления числителя на знаменатель - m % n = 1.
-    // Это число у нас будет выступать в качестве начального значения.
-    // 1) Сохраняем остаток в массиве. Умножаем остаток (массив не трогаем) на основание системы счисления (у нас десятичная) - 1*10 = 10
-    // 2) Находим остаток от деления числа с шага [1] на знаменатель - 10 % 3 = 1
-    // 3) Если он равен нулю, то дробь конечная. Иначе ищем остаток в массиве.
-    // Если нашли, то мы нашли период. Иначе берём текущий остаток и переходим к шагу [1].
+{ 
     int numerator, denominator;
     double temp = num;
     numerator = (int)num;
@@ -38,58 +72,16 @@ enum ans checkNum(double num, int base)
         numerator = (int)temp;
         denominator *= 10;
     }
-    int fraction = numerator;         // 0.12 числитель
-    int ost = fraction % denominator; // Находим остаток от деления числителя на знаменатель - m % n = 1.
-
-    int indexMas = 0;
-    int *mas = (int *)malloc(sizeof(int) * 10);
-    if (!mas)
-    {
-        return Error_memory;
-    }
-    int len = 10;
-    mas[indexMas] = ost; // Сохраняем остаток в массиве.
-    indexMas++;
-    int res = ost * base; // Умножаем остаток (массив не трогаем) на основание системы счисления 1
-
-    int whole_res = res % denominator; // Находим остаток от деления числа с шага [1] на знаменатель - 10 % 3 = 1 2
-    while (whole_res != 0)             // 3
-    {
-        int i = 0;
-        while(mas[i])
-        {
-            if (mas[i] == whole_res)
-            {
-                free(mas);
-                return No;
-            }
-            i++;
-        }
-        if (indexMas >= len)
-        {
-            int *temp = (int *)malloc(sizeof(int) * (len * 2));
-            if (!temp)
-            {
-                free(mas);
-                return Error_memory;
-            }
-            free(mas);
-            mas = temp;
-            len *= 2;
-        }
-        mas[indexMas] = whole_res;
-        indexMas++;
-        mas[indexMas] = '\0';
-        whole_res *= base;
-        whole_res = whole_res % denominator;
-    }
-    free(mas);
-    return Yes;
+    int nod = find_NOD(numerator, denominator);
+    numerator /= nod;
+    denominator /= nod;
+    //printf("%f %d %d\n", num, numerator, denominator);
+    return check_prime_devisors(denominator, base);
 }
-enum memory_check foo(struct res **mas, int ss, int n, ...)
+enum memory_check foo(struct res **mas, int ss, int n, ...) // 0 1 2  3
 {
-    *mas = malloc(sizeof(struct res) * n);
-    if (!mas)
+    *mas = malloc(sizeof(struct res) * (n + 1));
+    if (!(*mas))
     {
         return ERROR;
     }
@@ -114,8 +106,8 @@ enum memory_check foo(struct res **mas, int ss, int n, ...)
 int main()
 {
     struct res *mas;
-    int base = 10;
-    enum memory_check res = foo(&mas, base, 3, 2.3455, 0.33, 0.5);
+    int base = 8;
+    enum memory_check res = foo(&mas, base, 3, 0.5, 0.33, 0.1);
     if (res == ERROR)
     {
         printf("Не удалось выделить память\n");
