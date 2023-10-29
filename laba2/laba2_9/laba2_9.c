0c15f7257cf78864b338f3aee68a22f4d4825567
Chernysheva Sofya <128961179+SChernysheva@users.noreply.github.com>
2023-10-29 09:49:28 +0300

    change the algorithm

--- laba2/laba2_9/laba2_9.c
+++ laba2/laba2_9/laba2_9.c
@@ -5,7 +5,6 @@
 #include <ctype.h>
 #include <limits.h>
 #include <stdarg.h>
-// 3/ 10 -16 == 4 
 enum memory_check
 {
     ERROR,
@@ -14,40 +13,78 @@ enum memory_check
 enum ans
 {
     Yes,
-    No
+    No,
+    Error_memory
 };
 struct res
 {
     int ans;
     double num;
 };
-enum ans checkNum(double num, int base) 
-{
-    int whole_part = num / 1; 
-    double fraction = num - whole_part; 
-    double res = fraction * base; 
-    int whole_res = res / 1; 
-    double fraction_res = res - whole_res; 
-    int count = 0;
-    int mas[10];
-    for (int i = 0; i < 10; i++)
+enum ans checkNum(double num, int base)
+{ // Находим остаток от деления числителя на знаменатель - m % n = 1.
+    // Это число у нас будет выступать в качестве начального значения.
+    // 1) Сохраняем остаток в массиве. Умножаем остаток (массив не трогаем) на основание системы счисления (у нас десятичная) - 1*10 = 10
+    // 2) Находим остаток от деления числа с шага [1] на знаменатель - 10 % 3 = 1
+    // 3) Если он равен нулю, то дробь конечная. Иначе ищем остаток в массиве.
+    // Если нашли, то мы нашли период. Иначе берём текущий остаток и переходим к шагу [1].
+    int numerator, denominator;
+    double temp = num;
+    numerator = (int)num;
+    denominator = 1;
+    while (numerator != temp)
     {
-        mas[i] = 0;
+        temp *= 10;
+        numerator = (int)temp;
+        denominator *= 10;
     }
-    while (fraction_res != 0)
-    {
-        fraction_res *= base; 
-        int whole_part = fraction_res / 1;
-        fraction_res = fraction_res - whole_part;
-        count++;
-        if (mas[(int)(fraction_res * 10)] == 1)
-        {
-            return No;
-        }
-        mas[(int)(fraction_res * 10)] = 1;
-    }
-    return Yes;
+    int fraction = numerator;         // 0.12 числитель
+    int ost = fraction % denominator; // Находим остаток от деления числителя на знаменатель - m % n = 1.
 
+    int indexMas = 0;
+    int *mas = (int *)malloc(sizeof(int) * 10);
+    if (!mas)
+    {
+        return Error_memory;
+    }
+    int len = 10;
+    mas[indexMas] = ost; // Сохраняем остаток в массиве.
+    indexMas++;
+    int res = ost * base; // Умножаем остаток (массив не трогаем) на основание системы счисления 1
+
+    int whole_res = res % denominator; // Находим остаток от деления числа с шага [1] на знаменатель - 10 % 3 = 1 2
+    while (whole_res != 0)             // 3
+    {
+        int i = 0;
+        while(mas[i])
+        {
+            if (mas[i] == whole_res)
+            {
+                free(mas);
+                return No;
+            }
+            i++;
+        }
+        if (indexMas >= len)
+        {
+            int *temp = (int *)malloc(sizeof(int) * (len * 2));
+            if (!temp)
+            {
+                free(mas);
+                return Error_memory;
+            }
+            free(mas);
+            mas = temp;
+            len *= 2;
+        }
+        mas[indexMas] = whole_res;
+        indexMas++;
+        mas[indexMas] = '\0';
+        whole_res *= base;
+        whole_res = whole_res % denominator;
+    }
+    free(mas);
+    return Yes;
 }
 enum memory_check foo(struct res **mas, int ss, int n, ...)
 {
@@ -61,8 +98,11 @@ enum memory_check foo(struct res **mas, int ss, int n, ...)
     for (int i = 0; i < n; i++)
     {
         double num = va_arg(args, double);
-        printf("%f\n", num);
-        enum ans ans= checkNum(num, ss);
+        enum ans ans = checkNum(num, ss);
+        if (ans == Error_memory)
+        {
+            return ERROR;
+        }
         (*mas)[i].ans = (ans == Yes) ? 1 : 0;
         (*mas)[i].num = num;
     }
@@ -73,17 +113,18 @@ enum memory_check foo(struct res **mas, int ss, int n, ...)
 
 int main()
 {
-    struct res* mas;
-    enum memory_check res = foo(&mas, 15, 3, 0.5, 0.2, 0.3);
+    struct res *mas;
+    int base = 10;
+    enum memory_check res = foo(&mas, base, 3, 2.3455, 0.33, 0.5);
     if (res == ERROR)
     {
         printf("Не удалось выделить память\n");
     }
     int i = 0;
-    while(mas[i].ans != -1)
+    while (mas[i].ans != -1)
     {
         printf("Для дроби %f конечное представление ", mas[i].num);
-        (mas[i].ans == 1) ? printf("определено\n") : printf("не определено\n");
+        (mas[i].ans == 1) ? printf("определено\n") : printf("не определено\n"); // 2,345 / 10
         i++;
     }
     free(mas);
