35bae8d964b1968d0d51234a9fdc6ac5c46c211e
Chernysheva Sofya <128961179+SChernysheva@users.noreply.github.com>
2023-11-22 20:29:58 +0300

    Add files via upload
    Done!!

--- laba4/laba4_2.c
+++ laba4/laba4_2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h> 
+#include <time.h>
 #include <ctype.h>
 #include <limits.h>
 #include <stdarg.h>
@@ -11,38 +11,43 @@ enum checker
     ERROR_MEMORY,
     INVALID
 };
-typedef struct 
+typedef struct
 {
-    int* array; 
+    int *array;
     int capasity;
     int size;
     char name;
 } mas;
-mas* create_mas(int capasity, char name)
+mas *create_mas(int capasity, char name)
 {
-    mas* res = malloc(sizeof(mas));
+    mas *res = malloc(sizeof(mas));
     if (!res)
     {
         return NULL;
     }
-    res->array = malloc(sizeof(int) * capasity);
+    res->array = (int *)malloc(sizeof(int) * capasity);
+    if (!res->array)
+        return NULL;
     res->capasity = capasity;
     res->name = name;
     res->size = 0;
     return res;
 }
-int checkInt(char* str)
+int checkInt(char *str)
 {
-    while(*str)
+    if (str == NULL)
+        return 0;
+    while (*str)
     {
-        if (!isdigit(*str)) return 0;
+        if (!isdigit(*str))
+            return 0;
         str++;
     }
     return 1;
 }
-enum checker change_capasity(mas* data, int user_capasity)
+enum checker change_capasity(mas *data, int user_capasity)
 {
-    int* temp = realloc(data->array, sizeof(int) * user_capasity);
+    int *temp = realloc(data->array, sizeof(int) * user_capasity);
     if (!temp)
     {
         return ERROR_MEMORY;
@@ -52,13 +57,15 @@ enum checker change_capasity(mas* data, int user_capasity)
     return OK;
 }
 
-enum checker fLoad(char* str, mas** all_mass)
+enum checker fLoad(char *str, mas **all_mass)
 { // A, in.txt
-    if (!isalpha(str[0])) return INVALID;
-    if (str[1] != ',' || str[2] != ' ') return INVALID;
+    if (!isalpha(str[0]))
+        return INVALID;
+    if (str[1] != ',' || str[2] != ' ')
+        return INVALID;
     char name_mas = toupper(str[0]);
-    mas* arr = all_mass[(int)name_mas - 65];
-    FILE* file = fopen(&(str[3]), "r");
+    mas *arr = all_mass[(int)name_mas - 65];
+    FILE *file = fopen(&(str[3]), "r");
     if (!file)
     {
         return INVALID;
@@ -70,7 +77,7 @@ enum checker fLoad(char* str, mas** all_mass)
     {
         if (isdigit(c))
         {
-            while(isdigit(c))
+            while (isdigit(c))
             {
                 num[index_num] = c;
                 index_num++;
@@ -95,13 +102,15 @@ enum checker fLoad(char* str, mas** all_mass)
     return OK;
 }
 
-enum checker fRand(char* str, mas** all_mass)
-{//A, count, lb, rb
-    if (!isalpha(str[0])) return INVALID;
-    if (str[1] != ',' || str[2] != ' ') return INVALID;
+enum checker fRand(char *str, mas **all_mass)
+{ // A, count, lb, rb
+    if (!isalpha(str[0]))
+        return INVALID;
+    if (str[1] != ',' || str[2] != ' ')
+        return INVALID;
     char name_mas = toupper(str[0]);
-    mas* arr = all_mass[(int)name_mas - 65];
-    char* str_after_mas = &(str[3]);
+    mas *arr = all_mass[(int)name_mas - 65];
+    char *str_after_mas = &(str[3]);
     int i = 0;
     char first_num[10], second_num[10], count[5];
     while (isdigit(str_after_mas[i]))
@@ -110,8 +119,11 @@ enum checker fRand(char* str, mas** all_mass)
         i++;
     }
     count[i] = '\0';
-    if (!checkInt(count)) return INVALID;
+    if (!checkInt(count))
+        return INVALID;
     int num_count = atoi(count);
+    if (num_count >= 100000)
+        return ERROR_MEMORY;
     if (num_count + arr->size >= arr->capasity)
     {
         if (change_capasity(arr, arr->capasity + num_count) != OK)
@@ -128,7 +140,8 @@ enum checker fRand(char* str, mas** all_mass)
         i++;
     }
     first_num[index_] = '\0';
-    if (!checkInt(first_num)) return INVALID;
+    if (!checkInt(first_num))
+        return INVALID;
     int num_first = atoi(first_num);
     i += 2;
     index_ = 0;
@@ -139,11 +152,13 @@ enum checker fRand(char* str, mas** all_mass)
         i++;
     }
     second_num[index_] = '\0';
-    if (!checkInt(second_num)) return INVALID;
+    if (!checkInt(second_num))
+        return INVALID;
     int num_second = atoi(second_num);
-    if (num_first > num_second) return INVALID;
+    if (num_first > num_second)
+        return INVALID;
     srand(time(0));
-    printf("%d %d %d\n", num_count, num_first, num_second);
+    // printf("%d %d %d\n", num_count, num_first, num_second);
     for (int k = 0; k < num_count; k++)
     {
         arr->array[arr->size] = rand() % (num_second + 1) + num_first;
@@ -152,16 +167,20 @@ enum checker fRand(char* str, mas** all_mass)
     return OK;
 }
 
-enum checker fConcat(char* str, mas** all_mass)
-{//A, b;
-    if (!isalpha(str[0])) return INVALID;
-    if (str[1] != ',' || str[2] != ' ') return INVALID;
-    if (!isalpha(str[3])) return INVALID;
-    if (strlen(str) != 4) return INVALID;
+enum checker fConcat(char *str, mas **all_mass)
+{ // A, b;
+    if (!isalpha(str[0]))
+        return INVALID;
+    if (str[1] != ',' || str[2] != ' ')
+        return INVALID;
+    if (!isalpha(str[3]))
+        return INVALID;
+    if (strlen(str) != 4)
+        return INVALID;
     char name_mas = toupper(str[0]);
     char name_mas2 = toupper(str[3]);
-    mas* arr = all_mass[(int)name_mas - 65];
-    mas* arr2 = all_mass[(int)name_mas2 - 65];
+    mas *arr = all_mass[(int)name_mas - 65];
+    mas *arr2 = all_mass[(int)name_mas2 - 65];
     if (arr->capasity < arr->size + arr2->size)
     {
         if (change_capasity(arr, arr->size + arr2->size + 1) != OK)
@@ -177,13 +196,16 @@ enum checker fConcat(char* str, mas** all_mass)
     }
     return OK;
 }
-enum checker fRemove(char* str, mas** al_mass)
-{//a, 2, 7
-    if (!isalpha(str[0])) return INVALID;
-    if (str[1] != ',' || str[2] != ' ') return INVALID;
-    if (!isdigit(str[3])) return INVALID;
+enum checker fRemove(char *str, mas **al_mass)
+{ // a, 2, 7
+    if (!isalpha(str[0]))
+        return INVALID;
+    if (str[1] != ',' || str[2] != ' ')
+        return INVALID;
+    if (!isdigit(str[3]))
+        return INVALID;
     char name_mas = toupper(str[0]);
-    mas* arr = al_mass[(int)name_mas - 65];
+    mas *arr = al_mass[(int)name_mas - 65];
     char num1[3];
     char num2[3];
     int i = 3;
@@ -200,26 +222,30 @@ enum checker fRemove(char* str, mas** al_mass)
         index_num_2++;
     }
     num2[index_num_2] = '\0';
-    if (!checkInt(num1) || !checkInt(num2)) return INVALID;
-    int num_index = atoi(num1); //2
-    int num_count = atoi(num2);// 3;
-    if (num_index > arr->size || num_index < 0 || num_count < 0 || num_index + num_count > arr->size) return ERROR_MEMORY;
+    if (!checkInt(num1) || !checkInt(num2))
+        return INVALID;
+    int num_index = atoi(num1); // 2
+    int num_count = atoi(num2); // 3;
+    if (num_index > arr->size || num_index < 0 || num_count < 0 || num_index + num_count > arr->size)
+        return ERROR_MEMORY;
 
     int index_start = num_index;
-    for (int i = index_start; i < arr->size - num_count; i++) 
+    for (int i = index_start; i < arr->size - num_count; i++)
     {
         arr->array[i] = arr->array[i + num_count];
     }
     arr->size = arr->size - num_count;
     return OK;
 }
-enum checker fSave(char* str, mas** all_mass)
+enum checker fSave(char *str, mas **all_mass)
 {
-    if (!isalpha(str[0])) return INVALID;
-    if (str[1] != ',' || str[2] != ' ') return INVALID;
+    if (!isalpha(str[0]))
+        return INVALID;
+    if (str[1] != ',' || str[2] != ' ')
+        return INVALID;
     char name_mas = toupper(str[0]);
-    mas* arr = all_mass[(int)name_mas - 65];
-    FILE* file = fopen(&(str[3]), "w");
+    mas *arr = all_mass[(int)name_mas - 65];
+    FILE *file = fopen(&(str[3]), "w");
     if (!file)
     {
         return INVALID;
@@ -231,18 +257,19 @@ enum checker fSave(char* str, mas** all_mass)
     fclose(file);
     return OK;
 }
-//A, 4, 10, b; - скопировать из массива А элементы с 4 по 10 (оба конца
-//включительно) и сохранить их в b;
-enum checker fCopy(char* str, mas** all_mass)
+// A, 4, 10, b; - скопировать из массива А элементы с 4 по 10 (оба конца
+// включительно) и сохранить их в b;
+enum checker fCopy(char *str, mas **all_mass)
 {
-    if (!isalpha(str[0]) || str[1] != ',' || str[2] != ' ' || !isdigit(str[3])) return INVALID;
+    if (!isalpha(str[0]) || str[1] != ',' || str[2] != ' ' || !isdigit(str[3]))
+        return INVALID;
     char name_mas = toupper(str[0]);
-    mas* arr1 = all_mass[(int)name_mas - 65];
+    mas *arr1 = all_mass[(int)name_mas - 65];
     char first_num[10];
     char second_num[10];
-    char* str_after_name = &(str[3]);
+    char *str_after_name = &(str[3]);
     int i = 0;
-    while(isdigit(str_after_name[i]))
+    while (isdigit(str_after_name[i]))
     {
         first_num[i] = str_after_name[i];
         i++;
@@ -257,14 +284,17 @@ enum checker fCopy(char* str, mas** all_mass)
         index_sec_num++;
     }
     second_num[index_sec_num] = '\0';
-    if (strlen(first_num) == 0 || strlen(second_num) == 0) return INVALID;
+    if (strlen(first_num) == 0 || strlen(second_num) == 0)
+        return INVALID;
     i += 2;
-    if (!isalpha(str_after_name[i])) return INVALID;
+    if (!isalpha(str_after_name[i]))
+        return INVALID;
     char name_mas2 = toupper(str_after_name[i]);
-    mas* arr2 = all_mass[(int)name_mas2 - 65];
+    mas *arr2 = all_mass[(int)name_mas2 - 65];
     int index1 = atoi(first_num);
     int index_2 = atoi(second_num);
-    if (index1 > index_2 || index_2 >= arr1->size || index1 < 0 ) return ERROR_MEMORY;
+    if (index1 > index_2 || index_2 >= arr1->size || index1 < 0)
+        return ERROR_MEMORY;
     if (arr2->size + (index_2 - index1) + 1 >= arr2->capasity)
     {
         if (change_capasity(arr2, arr2->capasity + (index_2 - index1) + 1) != OK)
@@ -280,30 +310,37 @@ enum checker fCopy(char* str, mas** all_mass)
     }
     return OK;
 }
-int compareUp(const void * a, const void * b) {
-    return (*(int*)a - *(int*)b);
-}
-int compareDown(const void * a, const void * b) {
-    return (*(int*)b - *(int*)a);
-}
-enum checker fSort(char* str, mas** all_mass)
+int compareUp(const void *a, const void *b)
 {
-    if (strlen(str) > 2 || strlen(str) < 2) return INVALID;
-    if (!isalpha(str[0])) return INVALID;
-    if (str[1] != '+' && str[1] != '-') return INVALID;
+    return (*(int *)a - *(int *)b);
+}
+int compareDown(const void *a, const void *b)
+{
+    return (*(int *)b - *(int *)a);
+}
+enum checker fSort(char *str, mas **all_mass)
+{
+    if (strlen(str) > 2 || strlen(str) < 2)
+        return INVALID;
+    if (!isalpha(str[0]))
+        return INVALID;
+    if (str[1] != '+' && str[1] != '-')
+        return INVALID;
     char name_mas = toupper(str[0]);
-    mas* arr = all_mass[(int)name_mas - 65];
+    mas *arr = all_mass[(int)name_mas - 65];
     if (str[1] == '+')
     {
         qsort(arr->array, arr->size, sizeof(int), compareUp);
     }
-    else qsort(arr->array, arr->size, sizeof(int), compareDown);
-    return OK;   
+    else
+        qsort(arr->array, arr->size, sizeof(int), compareDown);
+    return OK;
 }
-enum checker fShuffle(char* str, mas** all_mass)
+enum checker fShuffle(char *str, mas **all_mass)
 {
-    if (strlen(str) != 1 || !isalpha(str[0])) return INVALID;
-    mas* arr = all_mass[(int)(toupper(str[0]) - 65)];
+    if (strlen(str) != 1 || !isalpha(str[0]))
+        return INVALID;
+    mas *arr = all_mass[(int)(toupper(str[0]) - 65)];
     qsort(arr->array, arr->size, sizeof(int), compareUp);
     for (int i = arr->size - 2; i > 0; i--)
     {
@@ -314,22 +351,25 @@ enum checker fShuffle(char* str, mas** all_mass)
     }
     return OK;
 }
-double find_max_away(int* array, int size, double middle_value)
+double find_max_away(int *array, int size, double middle_value)
 {
     double max_away = 0;
     for (int i = 0; i < size; i++)
     {
-        if (abs(array[i] - middle_value) > max_away) max_away = array[i] - middle_value;
+        if (abs(array[i] - middle_value) > max_away)
+            max_away = array[i] - middle_value;
     }
     return max_away;
 }
-enum checker fStats(char* str, mas** all_mass, int* size_mas, int* max_min, int* max_min_in, int* most_met, double* middle_value, double* max_away)
+enum checker fStats(char *str, mas **all_mass, int *size_mas, int *max_min, int *max_min_in, int *most_met, double *middle_value, double *max_away)
 {
-    if (strlen(str) != 1 || !isalpha(str[0]) ) return INVALID;
-    mas* arr = all_mass[(int)(toupper(str[0]) - 65)];
-    if (arr->size == 0) return INVALID;
+    if (strlen(str) < 1 || !isalpha(str[0]) || strlen(str) > 2)
+        return INVALID;
+    mas *arr = all_mass[(int)(toupper(str[0]) - 65)];
+    if (arr->size == 0)
+        return INVALID;
     *size_mas = arr->size;
-    int (*count)[2] = malloc(sizeof(int[2]) * arr->size); //0 - count, 1-value
+    int(*count)[2] = malloc(sizeof(int[2]) * arr->size); // 0 - count, 1-value
     int index_count = 0;
     if (!count)
     {
@@ -375,7 +415,7 @@ enum checker fStats(char* str, mas** all_mass, int* size_mas, int* max_min, int*
             }
             index_count++;
         }
-        if (arr->array[i] > max_min[1]) 
+        if (arr->array[i] > max_min[1])
         {
             max_min[1] = arr->array[i];
             max_min_in[1] = i;
@@ -391,17 +431,19 @@ enum checker fStats(char* str, mas** all_mass, int* size_mas, int* max_min, int*
     free(count);
     return OK;
 }
-enum checker fPrintf(char* str, mas** all_mass)
-{//a, 3
-    if (!isalpha(str[0])) return INVALID;
+enum checker fPrintf(char *str, mas **all_mass)
+{ // a, 3
+    if (!isalpha(str[0]))
+        return INVALID;
     char name_mas = toupper(str[0]);
-    if (str[1] != ',') return INVALID;
-    mas* arr = all_mass[(int)name_mas - 65];
+    if (str[1] != ',')
+        return INVALID;
+    mas *arr = all_mass[(int)name_mas - 65];
     char first_num[4];
     char second_num[4];
-    char* str_after_print = &(str[3]);
+    char *str_after_print = &(str[3]);
     int i = 0;
-    if (strcmp(str_after_print, "all") == 0)
+    if (strncmp(str_after_print, "all", 3) == 0)
     {
         for (int i = 0; i < arr->size; i++)
         {
@@ -422,7 +464,8 @@ enum checker fPrintf(char* str, mas** all_mass)
             return INVALID;
         }
         int index = atoi(first_num);
-        if (index < 0 || index > arr->size) return ERROR_MEMORY;
+        if (index < 0 || index >= arr->size)
+            return ERROR_MEMORY;
         printf("%d\n", arr->array[index]);
         return OK;
     }
@@ -443,43 +486,47 @@ enum checker fPrintf(char* str, mas** all_mass)
         }
         int index_1 = atoi(first_num);
         int index_2 = atoi(second_num);
-        if (index_2 < index_1 || index_2 > arr->size || index_1 < 0) return ERROR_MEMORY;
+        if (index_2 < index_1 || index_2 >= arr->size || index_1 < 0)
+            return ERROR_MEMORY;
         for (int m = index_1; m <= index_2; m++)
         {
             printf("%d ", arr->array[m]);
         }
         return OK;
-    } 
+    }
 }
-enum checker fFree(char* str, mas** all_mass)
+enum checker fFree(char *str, mas **all_mass)
 {
-    if (strlen(str) != 2 || !isalpha(str[0])) return INVALID;
-    mas* arr = all_mass[(int)(toupper(str[0])) - 65];
-    if (arr->array != NULL) free(arr->array);
+    if (strlen(str) != 2 || !isalpha(str[0]))
+        return INVALID;
+    mas *arr = all_mass[(int)(toupper(str[0])) - 65];
+    if (arr->array != NULL)
+        free(arr->array);
     arr->array = NULL;
     arr->capasity = 0;
     arr->size = 0;
     return OK;
 }
-void free_all(mas** all_mass)
+void free_all(mas **all_mass)
 {
     for (int i = 0; i < 26; i++)
     {
-        if (all_mass[i]->array != NULL) 
+        if (all_mass[i]->array != NULL)
         {
             free(all_mass[i]->array);
             all_mass[i]->capasity = 0;
             all_mass[i]->size = 0;
         }
-        if (all_mass[i] != NULL) free(all_mass[i]);
+        if (all_mass[i] != NULL)
+            free(all_mass[i]);
     }
 }
 
-
-int main(char* argv[], int argc, ...)
+int main(int argc, char*argv[])
 {
-    mas* all_mass[26];
-    for (int i = 65; i < 91; i++) 
+    mas *all_mass[26];
+    char user_str[20];
+    for (int i = 65; i < 91; i++)
     {
         all_mass[i - 65] = create_mas(rand() % 20, (char)i);
         if (all_mass[i - 65] == NULL)
@@ -489,17 +536,41 @@ int main(char* argv[], int argc, ...)
             return 1;
         }
     }
-    char user_str[50];
+    int is_file = 0;
+    FILE *file;
     int k = 0;
-    printf("Введите инструкцию, print или exit для выхода\n");
-    char c;
-    while ((c = getchar()) != '\n')
+    if (argc > 1)
     {
-        user_str[k] = c;
-        k++;
+        if (!(file = fopen(argv[1], "r")))
+        {
+            printf("Не удалось открыть входной файл\n");
+            return 1;
+        }
+        is_file = 1;
     }
-    user_str[k] = '\0';
-    while (strcmp(user_str, "exit") != 0)
+    if (is_file)
+    {
+        char c = fgetc(file);
+        while (c != '\n' && c != EOF)
+        {
+            user_str[k] = c;
+            c = fgetc(file);
+            k++;
+        }
+        user_str[k] = '\0';
+    }
+    else
+    {
+        printf("Введите инструкцию, print или exit для выхода\n");
+        char c;
+        while ((c = getchar()) != '\n')
+        {
+            user_str[k] = c;
+            k++;
+        }
+        user_str[k] = '\0';
+    }
+    while (strcmp(user_str, "exit") != 0 && (strlen(user_str) > 0))
     {
         char instruction[7];
         int i = 0;
@@ -510,7 +581,7 @@ int main(char* argv[], int argc, ...)
         }
         int index_dop_info = i + 1;
         instruction[i] = '\0';
-        if (strcmp(instruction, "Load") == 0) //10
+        if (strcmp(instruction, "Load") == 0) // 10
         {
             if (fLoad(&(user_str[index_dop_info]), all_mass) != OK)
             {
@@ -522,7 +593,7 @@ int main(char* argv[], int argc, ...)
             }
         }
         else if (strcmp(instruction, "Save") == 0)
-        {//Save A, out.txt; - выгрузить элементы массива A в файл out.txt;
+        { // Save A, out.txt; - выгрузить элементы массива A в файл out.txt;
             enum checker ans = fSave(&(user_str[index_dop_info]), all_mass);
             if (ans == INVALID)
             {
@@ -585,7 +656,7 @@ int main(char* argv[], int argc, ...)
                 printf("Ошибка времени исполнения\n");
                 free_all(all_mass);
                 return 1;
-            }   
+            }
         }
         else if (strcmp(instruction, "Print") == 0)
         {
@@ -664,16 +735,34 @@ int main(char* argv[], int argc, ...)
                 printf("Успешно\n");
             }
         }
-        
-        int k = 0;
-        printf("Введите инструкцию, print или exit для выхода\n");
-        char c;
-        while ((c = getchar()) != '\n')
+        k = 0;
+        if (is_file)
         {
-            user_str[k] = c;
-            k++;
+            char c = fgetc(file);
+            int k = 0;
+            while (c != '\n' && c != EOF)
+            {
+                user_str[k] = c;
+                c = fgetc(file);
+                k++;
+            }
+            user_str[k] = '\0';
         }
-        user_str[k] = '\0';
+        else
+        {
+            int k = 0;
+            printf("Введите инструкцию, print или exit для выхода\n");
+            char c;
+            while ((c = getchar()) != '\n')
+            {
+                user_str[k] = c;
+                k++;
+            }
+            user_str[k] = '\0';
+        }
     }
     free_all(all_mass);
+    printf("ok\n");
+    if (is_file) fclose(file);
+    return 0;
 }
